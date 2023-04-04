dd8cd15a4866c498b9796ec34ee94a2fdc849d74
David Lee <lihongyi0512@gmail.com>
2023-04-05 03:36:19 +0800

    “blog-aggregator”

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	}))
+
+	r.Get("/readiness", readinessHandler)
+
+	r2 := chi.NewRouter()
+	r2.Mount("/v1", r)
+	// Serve static files from the root directory
+	r2.Handle("/", http.FileServer(http.Dir(".")))
+
+	srv := &http.Server{
+		Addr: ":" + port,
+	}
+	log.Printf("Serving files on port: %s\n", port)
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	respondwithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
+}
+
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondwithJSON(w, code, map[string]string{"error": msg})
+}
