b7bcd1a24dd8626150754a11d966d69f9ba03d16
David Lee <lihongyi0512@gmail.com>
2023-04-07 23:00:02 +0800

    “feed”

---
+++ sql/schema/001_feeds.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+CREATE TABLE feeds (
+  id UUID NOT NULL PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name TEXT NOT NULL,
+  url TEXT UNIQUE NOT NULL,
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
