3358da0e1ae1d9b3dd914dd14e83c1e7b0c838fd
David Lee <lihongyi0512@gmail.com>
2023-04-08 16:46:32 +0800

    “feed_follows”

--- sql/schema/004_feed_follows.sql
+++ sql/schema/004_feed_follows.sql
@@ -4,7 +4,8 @@ CREATE TABLE feed_follows (
   feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP NOT NULL,
+  UNIQUE (user_id, feed_id)
 );
 
 -- +goose Down
