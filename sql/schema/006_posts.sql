7e5bbf16d1ec2b9e8d0c6223306c7d06992aed33
David Lee <lihongyi0512@gmail.com>
2023-04-09 20:00:11 +0800

    “posts”

---
+++ sql/schema/006_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  published_at TIMESTAMP NOT NULL,
+  feed_id INTEGER NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
