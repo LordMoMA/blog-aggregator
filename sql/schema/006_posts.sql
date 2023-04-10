27eacac7a89ecca1772a7a1d6ceb16d45e3364ed
David Lee <lihongyi0512@gmail.com>
2023-04-11 02:07:37 +0800

    “posts”

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -2,8 +2,8 @@
 CREATE TABLE posts (
   id UUID PRIMARY KEY,
   feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
   url TEXT NOT NULL UNIQUE,
